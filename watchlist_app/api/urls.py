dc7b29beb9eb6058c06d0e7e337de08d7dc7d066
NEWUSER <rhysmwangi96@gmail.com>
2021-07-08 22:56:48 +0300

    serializers and views

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from watchlist_app.api.views import movie_list, movie_details
 
 urlpatterns = [
-    path('list/',movie_list, name='movie-list'),
+    path('list/', movie_list, name='movie-list'),
     path('<int:pk>', movie_details, name='movie-detail'),
 
 ]
