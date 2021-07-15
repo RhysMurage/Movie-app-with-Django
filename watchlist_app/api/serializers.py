24b454aa57283178063f90f56f9ff0be41cc7184
NEWUSER <rhysmwangi96@gmail.com>
2021-07-15 15:36:12 +0300

    nested serializers

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -2,21 +2,15 @@ from rest_framework import serializers
 from watchlist_app.models import StreamPlatform, WatchList
 
 class WatchListSerializer(serializers.ModelSerializer):
-    len_name = serializers.SerializerMethodField()
-
-
+    
     class Meta:
         model = WatchList
         fields = '__all__'
-        # fields = ['name','description','id']
-        # exclude = ['name']
-    
-    def get_len_name(self, object):
-        length = len(object.title)
-        return length
 
 
 class StreamPlatformSerializer(serializers.ModelSerializer):
+    watchlist = WatchListSerializer(many=True, read_only=True)
+
     class Meta:
         model = StreamPlatform
         fields = '__all__'
