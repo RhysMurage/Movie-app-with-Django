f042554930831381005498d8eb4105709b49b057
NEWUSER <rhysmwangi96@gmail.com>
2021-07-17 22:24:03 +0300

    Refactor views to overwrite queryset

--- watchlist_app/api/urls.py
+++ watchlist_app/api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from watchlist_app.api.views import (ReviewDetail,ReviewList)
+from watchlist_app.api.views import (ReviewDetail,ReviewList,ReviewCreate)
 
 urlpatterns = [
     # path('list/', WatchListAV.as_view(), name='movie-list'),
@@ -10,7 +10,8 @@ urlpatterns = [
     path('review', ReviewList.as_view(), name='stream-detail'),
     path('review/<int:pk>', ReviewDetail.as_view(), name='review-list'),
 
-    # path('stream/<int:pk>/review', StreamPlatformDetailAV.as_view(), name='stream-detail'),
-    # path('stream/review/<int:pk>', ReviewList.as_view(), name='review-list'),
+    path('stream/<int:pk>/review-create', ReviewCreate.as_view(), name='review-create'),
+    path('stream/<int:pk>/review', ReviewDetail.as_view(), name='review-list'),
+    path('stream/review/<int:pk>', ReviewList.as_view(), name='review-detail'),
 
 ]
